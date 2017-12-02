45036d52fc0cad1efd110a7990e05931b41cee0f
Johan Bowald <johan@bowald.se>
2017-12-02 10:33:12 +0100

    simplified Day2's part2

--- 02/part2.py
+++ 02/part2.py
@@ -1,1 +1,1 @@
-print sum([c for row in [[int(nr) for nr in line.split()] for line in open('input.txt').readlines()] for a in row for c in [a/b for b in row if a is not b and a % b == 0]])
+print sum([a/b for row in [[int(nr) for nr in line.split()] for line in open('input.txt').readlines()] for a in row for b in row if a is not b and a % b == 0])
